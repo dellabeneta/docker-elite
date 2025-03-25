ebcfc7e5b624626c354ffca6d2074d6e732adc09
dellabeneta <m.dellabeneta@gmail.com>
2025-03-24 22:14:51 -0300

    adc 02-container-logs e volume

--- 02-container-logs/nginx_volume_container.sh
+++ 02-container-logs/nginx_volume_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #criado por Michel Dellabeneta
-#script para o 01-primeiro-container
+#script para o 02-container-logs
 
 # 1. Baixar imagem
 echo "Baixando imagem Nginx..."
@@ -8,23 +8,27 @@ docker pull nginx
 
 # 2. Iniciar container
 echo "Iniciando container 'meu-servidor' na porta 8080..."
-docker run -d --name meu-servidor -p 8080:80 nginx
+docker run -d \
+  --name meu-servidor \
+  -p 8080:80 \
+  -v nginx_logs:/var/log/nginx \
+  nginx
 
 # Pequena pausa para inicialização
 sleep 2
 
 # 3. Listar containers
 echo "Containers em execução:"
-docker conainer ls
+docker container ls
 
-# 4. Testar o servidor
+4. Testar o servidor
 echo "Servidor Nginx está ON! Abra no navegador: http://localhost:8080"
-echo "Tempo para testar de 60 segundos"
+echo "Você tem 60 segundos para testar..."
 sleep 60
 
 echo "Tempo esgotado! Encerrando..."
 
-# 6. Parar e remover
+# 6. Parar e remover elementos
 echo "Parando container..."
 docker stop meu-servidor
 
@@ -34,8 +38,10 @@ docker container rm meu-servidor
 echo "Removendo a imagem..."
 docker image rm nginx:latest
 
+echo "O Volume será mantido..."
+
 # 7. Listar todos containers
 echo "Containers restantes:"
 docker container ls -a
 
-echo "Ciclo completo concluído! Container foi criado, usado e removido"
+echo "Show! O container foi criado, usado e removido"
